7c2385af7a7e28e11264cf3b94e103b95226220d
m81-ito-keisuke <larc_bump717kei@yahoo.co.jp>
2026-01-18 20:51:49 +0900

    1.0.5
    PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'maltinome-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon.png',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
