52cde7736f2f61f49f3d1af7f5cb8ed671a255fa
m81-ito-keisuke <larc_bump717kei@yahoo.co.jp>
2026-02-11 04:00:43 +0900

    sw

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+    // skipWaiting()を実行し、待機状態をスキップして即時にアクティブにする
+    self.skipWaiting();
+    
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => {
+            // clients.claim()を実行し、直ちにページをコントロールする
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
